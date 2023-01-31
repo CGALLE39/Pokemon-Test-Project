9e29f5708156b351d2fb8587c90a8f8de434478b
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-01-31 13:38:09 -0600

    css

--- js/pokedex.js
+++ js/pokedex.js
@@ -16,42 +16,50 @@ const colors = {
     fighting: '#E6E0D4',
     normal: '#F5F5F5'
 };
-
+const main_types = Object.keys(colors);
 
 const fetchPokemons = async () => {
-    for(let i=1; i<= pokemons_number; i++) {
+    for (let i = 1; i <= pokemons_number; i++) {
         await getPokemon(i);
     }
-}
+};
 
 const getPokemon = async id => {
-    const url =
-        `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const res = await fetch(url);
     const pokemon = await res.json();
     createPokemonCard(pokemon);
-}
-
-fetchPokemons();
+};
 
 function createPokemonCard(pokemon) {
     const pokemonEl = document.createElement('div');
     pokemonEl.classList.add('pokemon');
 
+    const poke_types = pokemon.types.map(type => type.type.name);
+    const type = main_types.find(type => poke_types.indexOf(type) > -1);
     const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    const color = colors[type];
+
+    pokemonEl.style.backgroundColor = color;
 
     const pokeInnerHTML = `
-    <div class= "img-container">
-    <img src="https://pokeres.bastionbot.org/images/pokemon/1.png"
-    </div>
-    <div class= "info">
-    <span class="number">${pokemon.id}</span>
-    <h3 class="name">${name}</h3>
-    <small class="type">Type: <span>${type}</span></small>
-    </div>
+        <div class="img-container">
+            <img src="https://pokeres.bastionbot.org/images/pokemon/${
+        pokemon.id
+    }.png" alt="${name}" />
+        </div>
+        <div class="info">
+            <span class="number">#${pokemon.id
+        .toString()
+        .padStart(3, '0')}</span>
+            <h3 class="name">${name}</h3>
+            <small class="type">Type: <span>${type}</span></small>
+        </div>
     `;
 
     pokemonEl.innerHTML = pokeInnerHTML;
 
     poke_container.appendChild(pokemonEl);
 }
+
+fetchPokemons();
